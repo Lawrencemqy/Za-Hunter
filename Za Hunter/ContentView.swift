e9521c2a04d90e4489ce3c5438e2501460ed05ed
Lawrencemqy <1346847095@qq.com>
2021-07-31 22:02:01 +0800

    Initial Commit

---
+++ Za Hunter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Za Hunter
+//
+//  Created by 梅清阳 on 2021/7/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by 梅清阳 on 2021/7/31.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
